0dbc43621c5e0d6f97c7c5409bc5c72f5583bcc8
Milan Horvath <horvath.milcsi@gmail.com>
2024-01-06 00:19:25 +0100

    security group + network interface + elastic IP + Centos + nginx

--- main.tf
+++ main.tf
@@ -77,6 +77,7 @@ resource "aws_route_table" "webserver_route_table_20240105" {
 resource "aws_subnet" "webserver_subnet_20240105" {
   vpc_id     = aws_vpc.webserver_vpc_20240105.id
   cidr_block = "10.10.1.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "Subnet for practice webserver 20240105"
@@ -84,9 +85,118 @@ resource "aws_subnet" "webserver_subnet_20240105" {
 }
 
 #5 associate subnet and route table
+
+resource "aws_route_table_association" "webserver_association_20240105" {
+  subnet_id      = aws_subnet.webserver_subnet_20240105.id
+  route_table_id = aws_route_table.webserver_route_table_20240105.id
+}
+
 #6 create security group
-#7 create netwoek interface ??
+
+resource "aws_security_group" "allow_web_traffic_20240105" {
+  name        = "allow_web_traffic_20240105"
+  description = "Allow web inbound traffic"
+  vpc_id      = aws_vpc.webserver_vpc_20240105.id
+
+  ingress {
+    description      = "https from all Internet!"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"] # open to everyone! #[aws_vpc.main.cidr_block]
+  }
+
+  ingress {
+    description      = "http from all Internet!"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"] # open to everyone! #[aws_vpc.main.cidr_block]
+  }
+
+  ingress {
+    description      = "ssh from all Internet!"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"] # open to everyone! #[aws_vpc.main.cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_all_internet_ssh_http_https"
+  }
+}
+
+#7 create network interface
+
+resource "aws_network_interface" "webserver_wetwork_interface_20240105" {
+  subnet_id       = aws_subnet.webserver_subnet_20240105.id
+  private_ips     = ["10.10.1.50"]
+  security_groups = [aws_security_group.allow_web_traffic_20240105.id]
+
+  tags = {
+    Name = "webserver_wetwork_interface_20240105"
+  }
+/*
+  attachment {
+    instance     = aws_instance.test.id
+    device_index = 1
+  }
+*/
+}
+
 #8 create an elastic IP
+
+resource "aws_eip" "webserver_eip_20240105" {
+  domain = "vpc"
+  network_interface = aws_network_interface.webserver_wetwork_interface_20240105.id
+  associate_with_private_ip = "10.10.1.50"
+  depends_on = [ aws_internet_gateway.gw ]
+  tags = {
+    Name = "webserver_eip_20240105"
+  }
+}
+
 #9 create Centos server and install nginx
-# The EC2 instance will be deafulted in region us-east-1 and from the most recent amazon-linux2 official image.
+# The EC2 instance will be defaulted in region us-east-1 and from the most recent amazon-linux2 official image.
 # Default Instance name will be practice-nginx, default instance type will be t2.micro.
+resource "aws_instance" "webserver_instance_20240105" {
+  ami = "ami-0aedf6b1cb669b4c7"
+  availability_zone = "us-east-1a"
+  instance_type = "t2.micro"
+  key_name = "webserver_project_key_pair_20240105"
+
+  network_interface {
+    device_index = 0
+    network_interface_id = aws_network_interface.webserver_wetwork_interface_20240105.id
+  }
+  tags = {
+    name = "webserver_instance_20240105"
+  }
+
+  user_data = <<-EOF
+  #!/bin/bash
+  sudo yum update
+  sudo yum install nginx
+  curl -I 127.0.0.1
+  sudo systemctl start nginx
+  sudo systemctl enable nginx
+  EOF
+
+}
+
+output "instance_id" {
+ value = aws_instance.webserver_instance_20240105.id
+}
+
+output "public_ip" {
+ value = aws_instance.webserver_instance_20240105.public_ip
+}
